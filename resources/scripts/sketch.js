2c8908e41f81aff0ebc2be2a183d85bbd947cbc4
Alejandro Ferrera <aleferrera@hotmail.com>
2017-02-01 12:42:26 -0600

    Reset graph when switching between automata type

--- resources/scripts/sketch.js
+++ resources/scripts/sketch.js
@@ -21,45 +21,47 @@ $('#set-name').click(function() {
     setStateName();
 });
 
-paper.on('blank:pointerclick', function(evt, x, y) {
-    if(selectedCell && !selectedCell.initial) selectedCell.attr({circle: {fill: '#5755a1'}});
-    selectedCell = null;
-    $('#toolbar').hide();
-    if(toolbarAction === 'insert') {
-        automaton.insertState({
-            position: { x: x, y: y },
-            size: { width: 60, height: 60 }
-        });
-    }
-});
+registerEventHandlers(paper, automaton);
 
-paper.on('cell:pointerdown', function(cellView, evt, x, y) {
-    if(toolbarAction === 'remove') {
-        automaton.removeState(cellView.model);
-        $('#toolbar').hide();
-        selectedCell = null;
-    }
-});
+// paper.on('blank:pointerclick', function(evt, x, y) {
+//     if(selectedCell && !selectedCell.initial) selectedCell.attr({circle: {fill: '#5755a1'}});
+//     selectedCell = null;
+//     $('#toolbar').hide();
+//     if(toolbarAction === 'insert') {
+//         automaton.insertState({
+//             position: { x: x, y: y },
+//             size: { width: 60, height: 60 }
+//         });
+//     }
+// });
 
-paper.on('cell:pointerclick', function(cellView, evt, x, y) {
-    console.log('click');
-    if(toolbarAction === 'select') {
-        if(cellView.model.isElement()) {
-            if(selectedCell && !selectedCell.initial) selectedCell.attr({circle: {fill: '#5755a1'}});
-            selectedCell = cellView.model;
-            selectedCell.attr({circle: {fill: 'green'}});
-            document.getElementById('initialCheckbox').checked = selectedCell.initial;
-            document.getElementById('finalCheckbox').checked = selectedCell.final;
-            $('#toolbar').show();
-        }
-    }
-});
+// paper.on('cell:pointerdown', function(cellView, evt, x, y) {
+//     if(toolbarAction === 'remove') {
+//         automaton.removeState(cellView.model);
+//         $('#toolbar').hide();
+//         selectedCell = null;
+//     }
+// });
 
-automaton.on('change:source change:target', function(link) {
-    if(link.get('source').id && link.get('target').id && !link.attributes.labels) {
-        setTransitionSymbol(link);
-    }
-});
+// paper.on('cell:pointerclick', function(cellView, evt, x, y) {
+//     console.log('click');
+//     if(toolbarAction === 'select') {
+//         if(cellView.model.isElement()) {
+//             if(selectedCell && !selectedCell.initial) selectedCell.attr({circle: {fill: '#5755a1'}});
+//             selectedCell = cellView.model;
+//             selectedCell.attr({circle: {fill: 'green'}});
+//             document.getElementById('initialCheckbox').checked = selectedCell.initial;
+//             document.getElementById('finalCheckbox').checked = selectedCell.final;
+//             $('#toolbar').show();
+//         }
+//     }
+// });
+
+// automaton.on('change:source change:target', function(link) {
+//     if(link.get('source').id && link.get('target').id && !link.attributes.labels) {
+//         setTransitionSymbol(link);
+//     }
+// });
 
 function setStateName() {
     let name = prompt('New name for state:');
@@ -112,38 +114,91 @@ function saveAutomaton() {
 }
 
 function changeAutomaton(item) {
-    // $('#' + currentAutomaton).removeClass('active');
-    // currentAutomaton = item.id;
-    // item.className = 'active';
-    // automaton.clear();
-    // paper.remove();
-    // setNewAutomaton(item.id);
+    $('#' + currentAutomaton).removeClass('active');
+    currentAutomaton = item.id;
+    item.className = 'active';
+    resetGraph(item);
+}
+
+function resetGraph(item) {
+    automaton.clear();
+    paper.remove();
+    toolbarAction = 'select';
+    selectedCell = null;
+    insertedAlphabet = null;
+    paper = null;
+    setNewAutomaton(item.id);
 }
 
 function setNewAutomaton(name) {
-    // switch(name) {
-    //     case 'DFA':
-    //         automaton = new DFA();
-    //         console.log('DFA');
-    //         break;
-        
-    //     case 'NFA':
-    //         automaton = new NFA();
-    //         console.log('NFA');
-    //         break;
-    // }
-    // paper = null;
-    // let div = document.createElement('paper');
-    // document.body.appendChild(div);
-    // console.log(document.getElementById('paper'));
+    automaton = generateNewAutomaton(name);
+    
+    let div = document.createElement('div');
+    div.id = 'paper';
+    document.body.appendChild(div);
 
-    // paper = new joint.dia.Paper({
-    // el: $('#paper'),
-    // width: 1800,
-    // height: 1000,
-    // gridSize: 1,
-    // model: automaton,
-    // defaultLink: new joint.shapes.fsa.Arrow
-    // });
-    // console.log(document.getElementById('paper'));
+    paper = new joint.dia.Paper({
+    el: $('#paper'),
+    width: 1800,
+    height: 1000,
+    gridSize: 1,
+    model: automaton,
+    defaultLink: new joint.shapes.fsa.Arrow
+    });
+
+    registerEventHandlers(paper, automaton);    
+}
+
+function generateNewAutomaton(name) {
+    switch(name) {
+        case 'DFA':
+            return new DFA();
+        
+        case 'NFA':
+            return new NFA();
+        
+        default:
+            return null;
+    }
+}
+
+function registerEventHandlers(paper, graph) {
+    paper.on('blank:pointerclick', function(evt, x, y) {
+        if(selectedCell && !selectedCell.initial) selectedCell.attr({circle: {fill: '#5755a1'}});
+        selectedCell = null;
+        $('#toolbar').hide();
+        if(toolbarAction === 'insert') {
+            automaton.insertState({
+                position: { x: x, y: y },
+                size: { width: 60, height: 60 }
+            });
+        }
+    });
+
+    paper.on('cell:pointerdown', function(cellView, evt, x, y) {
+        if(toolbarAction === 'remove') {
+            automaton.removeState(cellView.model);
+            $('#toolbar').hide();
+            selectedCell = null;
+        }
+    });
+
+    paper.on('cell:pointerclick', function(cellView, evt, x, y) {
+        if(toolbarAction === 'select') {
+            if(cellView.model.isElement()) {
+                if(selectedCell && !selectedCell.initial) selectedCell.attr({circle: {fill: '#5755a1'}});
+                selectedCell = cellView.model;
+                selectedCell.attr({circle: {fill: 'green'}});
+                document.getElementById('initialCheckbox').checked = selectedCell.initial;
+                document.getElementById('finalCheckbox').checked = selectedCell.final;
+                $('#toolbar').show();
+            }
+        }
+    });
+
+    graph.on('change:source change:target', function(link) {
+        if(link.get('source').id && link.get('target').id && !link.attributes.labels) {
+            setTransitionSymbol(link);
+        }
+    });
 }
