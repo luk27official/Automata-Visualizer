96328f6a76684ade057e5137a99ee19b63a0ee8d
Alejandro Ferrera <aleferrera@hotmail.com>
2017-02-20 11:02:39 -0600

    Apply material design

--- resources/scripts/diagramController.js
+++ resources/scripts/diagramController.js
@@ -17,6 +17,14 @@ var paper = generateNewPaper();
 setUp();
 
 $('#toolbar').hide();
+$(".button-collapse").sideNav();
+$('.button-collapse').sideNav({
+      menuWidth: 400, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
 
 $('#set-name').click(function() {
     if(!selectedCell) return;
