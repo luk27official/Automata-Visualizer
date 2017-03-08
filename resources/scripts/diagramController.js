2e85fe9e954bd7ea004b426d028d1df96046c6b1
Alejandro Ferrera <aleferrera@hotmail.com>
2017-03-08 11:15:02 -0600

    Move grammar insertion rule logic into its own controller

--- resources/scripts/diagramController.js
+++ resources/scripts/diagramController.js
@@ -195,15 +195,7 @@ function minimize() {
 }
 
 function addNewGrammarRule(e) {
-    let tr = null;
-
-    if(e.which != 13) return;
-    tr = $('#grammar-rules tbody tr:last-child td:last-child');
-    if(!tr.find('input').val()) return;
-
-
-    $('#grammar-rules > tbody:last-child').append('<tr><td><div class="input-field inline"><input placeholder="Production" type="text" autofocus></div></td><td><div class="input-field "><input type="text" value="&#x2192" disabled></div></td><td><div class="input-field inline"><input class="terminal" placeholder="Terminal" type="text"></div></td></tr>');
-    $('.terminal').keypress(addNewGrammarRule);
+    grammarCtrl.addNewRule(e);
 }
 
 function loadAutomatonInNewTab(newAutomaton, type) {
--- resources/scripts/grammarController.js
+++ resources/scripts/grammarController.js
@@ -1,14 +1,20 @@
 
 var grammarCtrl = (function() {
 
-function grammarCtrl() {
+function addNewRule(e) {
+    let tr = null;
 
+    if(e.which != 13) return;
+    tr = $('#grammar-rules tbody tr:last-child td:last-child');
+    if(!tr.find('input').val()) return;
+
+
+    $('#grammar-rules > tbody:last-child').append('<tr><td><div class="input-field inline"><input placeholder="Production" type="text" autofocus></div></td><td><div class="input-field "><input type="text" value="&#x2192" disabled></div></td><td><div class="input-field inline"><input class="terminal" placeholder="Terminal" type="text"></div></td></tr>');
+    $('.terminal').keypress(addNewGrammarRule);
 }
 
-function addNewRule() {
-    
+return {
+    addNewRule: addNewRule
 }
-
-return grammarCtrl;
 
 })();
